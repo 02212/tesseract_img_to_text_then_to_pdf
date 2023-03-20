eee05d5c72d7ca9b797b433ef336ed5da2d18920
khomotso joseph <219023456@tut4life.ac.za>
2023-03-21 01:05:04 +0200

    initial commit

--- text_dect.py
+++ text_dect.py
@@ -1,11 +1,10 @@
+import unicodedata
 import cv2
 import pytesseract
 from fpdf import FPDF
+import io
 
 text_to_pdf = FPDF()#take str to pd
-
-import unicodedata
-
 
 
 text_to_pdf.add_page() #add page
@@ -17,18 +16,26 @@ text_to_pdf.set_font("Arial",size = 14)
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 img = cv2.imread('img/2022-02-23_001.jpg')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+
 str_of_img = pytesseract.image_to_string(img)
 
 print(str_of_img)
 
+
 def strip_accents(str_of_img):
-    return "".join(char for char in
+    return ''.join(char for char in
                    unicodedata.normalize('NFKD', str_of_img)
                    if unicodedata.category(char) != 'Mn')
 
-strip_accents('áéíñóúü')
+with io.open('output_txt.txt',"w" ,encoding="utf-8") as i:
+    i.write(str_of_img)
+
+
+f = open("output_txt.txt",'r',encoding="utf8")
+
 #setup page sell
-for i in str_of_img:
+for i in f:
     text_to_pdf.cell(200, 10, txt = i, ln = 1, align = 'C')
 
 
